0a60d9169ddeec4454644ab924cec8620ed20ea7
Aphra Hiat <aphrahiat@Aphras-MBP.lan>
2023-04-17 21:09:06 -0700

    updated with some arbitrary changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import SearchBar from './components/SearchBar';
 import Gallery from './components/Gallery';
+import { DataContext } from './contexts/DataContext';
+import { SearchContext } from './contexts/SearchContext';
 
 import './App.css';
 
 function App() {
-  let [search, setSearch] = useState('');
+
   let [message, setMessage] = useState('Search for Music!!');
   let [data, setData] = useState([]);
+  let searchInput = useRef('')
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (!search) return
-      document.title = `${search} Music`
-      const response = await fetch(`https://itunes.apple.com/search?term=${search}`);
-      const resData = await response.json();
-      if (resData.results.length) {
-        setData(resData.results)
-      } else {
-          setData([]);
-          setMessage("Nothing found for that artist")
-      }
-
-      console.log(resData)
+  const handleSearch = async searchTerm => {
+    if (!searchTerm) return
+    document.title = `${searchTerm} Music`
+    const response = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`);
+    const resData = await response.json();
+    if (resData.results.length) {
+      setData(resData.results)
+    } else {
+      setData([]);
+      setMessage("Nothing found for that artist")
     }
-     fetchData();
-  }, [search]);
+
+    console.log(resData)
+  }
 
   return (
     <div className="App">
-     <SearchBar setSearch={ setSearch } />
-      { message }
-      <Gallery data={ data }/>
+      <SearchContext.Provider value={
+        {
+          term: searchInput,
+          handleSearch
+        }
+      }>
+        <SearchBar />
+      </SearchContext.Provider>
+
+      <DataContext.Provider value={
+        {
+          data
+        }
+      }>
+        {message}
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 }
